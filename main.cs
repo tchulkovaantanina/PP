9ffee79078f2d5aad09418713b897ec30a88e9e0
Chulkova Antonina <chylatosa2708@gmail.com>
2023-02-20 19:55:38 +0300

    Added comma.

--- main.cs
+++ main.cs
@@ -8,3 +8,32 @@ nemespace Git
 		}
 	}
 } 
+using System;
+
+namespace Git
+{
+	class Git 
+	{
+		static void Main(string[] arge)
+		{
+			System.Console.WriteLine("Enter your name: ");
+			string name = Consol.ReadLine();
+			System.Console.WriteLine(name.Length); //debug line
+			System.Console.WriteLine($"Hello {name}!");
+		}
+	}
+}
+using System;
+namespace Git 
+{
+	class Git 
+	{
+		static void Main(string[] arge)
+		{
+			System.Console.WriteLine("Enter your first name: ");
+			string first_name = Console.ReadLine();
+			Sestem.Console.WriteLine("Now enter your last name: ");
+			string last_name = Console.ReadLine();
+			System.Console.WriteLine($"Hello {first_name} {last_name}!");
+		}
+	}
